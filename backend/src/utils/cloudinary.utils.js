b4faa7325f6ca1954f89075caa97ac86db667418
Vipul Kumar Chauhan <vc252@snu.edu.in>
2025-01-25 01:13:47 +0530

    add user model

--- backend/src/utils/cloudinary.utils.js
+++ backend/src/utils/cloudinary.utils.js
@@ -6,7 +6,7 @@ import ApiError from "./ApiError.utils";
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const maxAttempts = 3;
