a3cd57fd02184f0ea424fbd5551a27311d4905f8
P4R1H <damps.home@gmail.com>
2025-01-30 02:12:32 +0530

    refactoring

---
+++ frontend/src/assets/data/sampleLb.ts
@@ -0,0 +1,40 @@
+import { LeaderboardEntry } from '../../types/Pages'
+
+export const mockLeaderboardData: LeaderboardEntry[] = [
+    {
+      rank: 1,
+      username: "PirateKing",
+      totalValue: 500000,
+      topStock: "Monkey D. Luffy",
+      profitPercentage: 156,
+    },
+    {
+      rank: 2,
+      username: "BerryHunter",
+      totalValue: 450000,
+      topStock: "Roronoa Zoro",
+      profitPercentage: 120,
+    },
+    {
+      rank: 3,
+      username: "NaviMaster",
+      totalValue: 400000,
+      topStock: "Nami",
+      profitPercentage: 95,
+    },
+    {
+      rank: 4,
+      username: "SwordCollector",
+      totalValue: 350000,
+      topStock: "Zoro",
+      profitPercentage: 82,
+    },
+    {
+      rank: 5,
+      username: "MeatLover",
+      totalValue: 300000,
+      topStock: "Sanji",
+      profitPercentage: 75,
+    },
+  ];
+  
