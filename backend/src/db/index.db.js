0deb548ea0468862622a38b2cdea861d1ddb6015
P4R1H <damps.home@gmail.com>
2025-01-30 16:46:35 +0530

    Mongodb URI parsing

--- backend/src/db/index.db.js
+++ backend/src/db/index.db.js
@@ -3,9 +3,8 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGO_URI}/${DB_NAME}`
-    );
+    const url = new URL(`${process.env.MONGO_URI}/${DB_NAME}`);
+    const connectionInstance = await mongoose.connect(url.toString());
     console.log(
       `MONGODB connected host:${connectionInstance.connection.host} `
     );
