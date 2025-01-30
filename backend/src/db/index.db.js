1caec0917f48db2228b99a92b79808665948119f
P4R1H <damps.home@gmail.com>
2025-01-30 18:04:50 +0530

    Login api

--- backend/src/db/index.db.js
+++ backend/src/db/index.db.js
@@ -4,6 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
   try {
     const url = new URL(`${process.env.MONGO_URI}/${DB_NAME}`);
+    console.log(url.toString());
     const connectionInstance = await mongoose.connect(url.toString());
     console.log(
       `MONGODB connected host:${connectionInstance.connection.host} `
