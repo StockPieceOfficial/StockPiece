a6eb6ee506155a6cb610bd4cdfac6b2731b1108b
P4R1H <damps.home@gmail.com>
2025-02-18 18:42:22 +0530

    Vercel endpoint fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,19 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
   server: {
     host: '0.0.0.0',
-    port: 5173, 
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'https://stockpiece.onrender.com',
+        target: process.env.VITE_API_BASE_URL || 'https://stockpiece.onrender.com',
         changeOrigin: true,
         secure: false,
       }
     }
   }
 })
-
-
