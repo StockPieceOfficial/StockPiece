f618252f9d4238d57be7e432ff69f4382f0d7b60
Vipul Kumar Chauhan <vc252@snu.edu.in>
2025-02-26 01:56:07 +0530

    auth improve

--- backend/src/middlewares/auth.middlewares.js
+++ backend/src/middlewares/auth.middlewares.js
@@ -4,8 +4,26 @@ import jwt from "jsonwebtoken";
 import User from "../models/user.models.js";
 import Admin from "../models/admin.models.js";
 
+const verifyToken = async (token, secret, model, selectFields) => {
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, secret);
+  } catch (err) {
+    if (err.name === "TokenExpiredError") {
+      return null;
+    }
+    throw new ApiError(401, "Invalid Access Token");
+  }
+
+  const user = await model.findById(decodedToken?._id).select(selectFields);
+  if (!user) {
+    throw new ApiError(401, "Invalid Access Token");
+  }
+
+  return user;
+};
+
 const verifyJWT = asyncHandler(async (req, _, next) => {
-  // Get the access token from cookies or the authorization header
   const accessToken =
     req.cookies?.accessToken ||
     req.headers.authorization?.replace("Bearer ", "");
@@ -14,31 +32,21 @@ const verifyJWT = asyncHandler(async (req, _, next) => {
     return next();
   }
 
-  let decodedToken;
-  try {
-    decodedToken = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
-  } catch (err) {
-    // If token has expired, simply call next() without attaching anything to req.
-    if (err.name === "TokenExpiredError") {
-      return next();
-    }
-    throw new ApiError(401, "Invalid Access Token");
-  }
-
-  const user = await User.findById(decodedToken?._id).select(
+  const user = await verifyToken(
+    accessToken,
+    process.env.ACCESS_TOKEN_SECRET,
+    User,
     "-password -refreshToken"
   );
 
-  if (!user) {
-    throw new ApiError(401, "Invalid Access Token");
+  if (user) {
+    req.user = user;
   }
 
-  req.user = user;
   next();
 });
 
 const verifyAdminJWT = asyncHandler(async (req, _, next) => {
-  // Get the admin token from cookies or the authorization header
   const adminToken =
     req.cookies?.adminToken ||
     req.headers.authorization?.replace("Bearer ", "");
@@ -47,27 +55,17 @@ const verifyAdminJWT = asyncHandler(async (req, _, next) => {
     return next();
   }
 
-  let decodedToken;
-  try {
-    decodedToken = jwt.verify(
-      adminToken,
-      process.env.ADMIN_ACCESS_TOKEN_SECRET
-    );
-  } catch (err) {
-    // If token has expired, simply call next() without attaching anything to req.
-    if (err.name === "TokenExpiredError") {
-      return next();
-    }
-    throw new ApiError(401, "Invalid Access Token");
+  const admin = await verifyToken(
+    adminToken,
+    process.env.ADMIN_ACCESS_TOKEN_SECRET,
+    Admin,
+    "-password"
+  );
+
+  if (admin) {
+    req.admin = admin;
   }
 
-  const admin = await Admin.findById(decodedToken?._id).select("-password");
-
-  if (!admin) {
-    throw new ApiError(401, "Invalid Access Token");
-  }
-
-  req.admin = admin;
   next();
 });
 
