cc6c0ce51d1285464c5cb36508d8ff71c0adf151
P4R1H <damps.home@gmail.com>
2025-02-25 15:14:38 +0530

    insights

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from '@vercel/analytics/react'
+
 import './index.css'
 import App from './App.tsx'
 
@@ -10,6 +13,8 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <SpeedInsights />
+      <Analytics />
     </QueryClientProvider>
   </StrictMode>,
 )
