2baabcbe77e9fa5cfe06e73cb1664ae50e0cd978
P4R1H <damps.home@gmail.com>
2025-02-26 22:10:00 +0530

    profit percentage fix

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -66,7 +66,7 @@ const HomePage: React.FC<HomePageProps> = ({ isLoggedIn }) => {
     gcTime: 1000 * 60 * 30
   });
 
-  const initialInvestment = 10000;
+  const initialInvestment = 2500;
   const portfolioStats = useMemo(() => {
     const netWorthValue = portfolio.stockValue + portfolio.cash;
     return {
