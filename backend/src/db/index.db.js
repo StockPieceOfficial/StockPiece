4f9e72d3d8790b7bbb3311bd1c98786a47e7b331
P4R1H <damps.home@gmail.com>
2025-02-26 17:20:41 +0530

    refresh token endpoint

--- backend/src/db/index.db.js
+++ backend/src/db/index.db.js
@@ -4,12 +4,8 @@ import { URL } from "node:url";
 
 const connectDB = async () => {
   try {
-<<<<<<< HEAD
     const url = new URL(`${process.env.MONGO_URI}/${DB_NAME}${"?retryWrites=true&w=majority&appName=roadmap"}`);
     console.log(url.toString());
-=======
-    const url = new URL(`${process.env.MONGO_URI}/${DB_NAME}`);
->>>>>>> f440129c807a0d30dab76c9fc2f07729f6cb406b
     const connectionInstance = await mongoose.connect(url.toString());
     console.log(
       `MONGODB connected host:${connectionInstance.connection.host} `
