802abac3c0299c4c61ec7f7025d63670795623eb
Vipul Kumar Chauhan <vc252@snu.edu.in>
2025-03-02 01:08:38 +0530

    bug fix

--- backend/src/utils/windowStatus.js
+++ backend/src/utils/windowStatus.js
@@ -1,9 +1,7 @@
 //i am trusting that will get a valid doc
 const isWindowOpen = (chapterDoc) => {
-  return (
-    !chapterDoc.isWindowClosed &&
-    Date.now() < chapterDoc.windowEndDate.getTime()
-  );
+  return !chapterDoc.isWindowClosed;
+  // && Date.now() < chapterDoc.windowEndDate.getTime()
 };
 
 export default isWindowOpen;
