b010bdbd050b3943457afadf2240fb7cef548a02
P4R1H <damps.home@gmail.com>
2025-03-07 00:36:15 +0530

    temp notif

--- frontend/src/utils/MarketStatus.ts
+++ frontend/src/utils/MarketStatus.ts
@@ -1,5 +1,6 @@
 // src/utils/marketStatus.ts
-export type MarketStatus = 'open' | 'closed' | 'updating';
+// export type MarketStatus = 'open' | 'closed' | 'updating';
+export type MarketStatus = any
 
 export interface MarketStatusInfo {
   status: MarketStatus;
@@ -76,6 +77,10 @@ export function getMarketStatusInfo(): MarketStatusInfo {
       timeUntilNext = `${daysUntilThursday} day${daysUntilThursday > 1 ? 's' : ''}`;
     }
   }
+
+  status = 'Closed';
+   nextStatus = 'Open';
+  timeUntilNext = 'Dont know, Fixing issues, Sorry!';
 
   return { status, nextStatus, timeUntilNext };
 }
