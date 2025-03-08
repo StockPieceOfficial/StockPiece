da0819e71a90e94cad501959a47f0b93ac533c79
Vipul Kumar Chauhan <vc252@snu.edu.in>
2025-03-09 05:04:38 +0530

    misc

--- backend/src/db/index.db.js
+++ backend/src/db/index.db.js
@@ -4,9 +4,9 @@ import { URL } from "node:url";
 
 const connectDB = async () => {
   try {
-    const url = new URL(`mongodb://root:nh5OQCgnrkX9AJ7kf8X8msbK5N2057mC7669QRv9iuh9Wi5JiHChbItNAXIarUqq@138.199.226.113:5432,138.199.226.113:5433,138.199.226.113:5434/?replicaSet=rs0/${DB_NAME}`);
+    const url = new URL(`${process.env.MONGO_URI}/${DB_NAME}`);
     console.log(url.toString());
-    const connectionInstance = await mongoose.connect(`mongodb://root:nh5OQCgnrkX9AJ7kf8X8msbK5N2057mC7669QRv9iuh9Wi5JiHChbItNAXIarUqq@138.199.226.113:5432,138.199.226.113:5433,138.199.226.113:5434/?replicaSet=rs0/${DB_NAME}`);
+    const connectionInstance = await mongoose.connect(url.toString());
     console.log(
       `MONGODB connected host:${connectionInstance.connection.host} `
     );
